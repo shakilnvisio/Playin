9414a1beafd2118f81e13fb05927d043cc63dbc5
Shakil Ahmed <shakil.sharin14@gmail.com>
2018-04-30 14:18:58 +0600

    Initial commit

---
+++ app/src/main/java/com/nvisio/project/playin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.nvisio.project.playin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.sensorberg.permissionbitte.BitteBitte;
+import com.sensorberg.permissionbitte.PermissionBitte;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
