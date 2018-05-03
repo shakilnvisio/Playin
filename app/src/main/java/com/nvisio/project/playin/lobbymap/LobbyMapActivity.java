62e685676cfbbdaf0888cb79be61d661aa9a6f13
Shakil Ahmed <shakil.sharin14@gmail.com>
2018-05-03 17:04:24 +0600

    Map integrated in LobbyMap not polished

--- app/src/main/java/com/nvisio/project/playin/lobbymap/LobbyMapActivity.java
+++ app/src/main/java/com/nvisio/project/playin/lobbymap/LobbyMapActivity.java
@@ -1,16 +1,34 @@
 package com.nvisio.project.playin.lobbymap;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.nvisio.project.playin.R;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class LobbyMapActivity extends AppCompatActivity {
+    private SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+    private OnMapReadyCallback onMapReadyCallback;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +39,47 @@ public class LobbyMapActivity extends AppCompatActivity {
             window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
         }
         setContentView(R.layout.lobby_map_activity);
+
+        onMapReadyCallback=this::MapReady;
+        MapInitialise();
+        LobbyMapActivityPermissionsDispatcher.CallMapWithPermissionCheck(LobbyMapActivity.this);
+        Log.d("log>>","start");
+
+
+
+    }
+
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION})
+    void CallMap(){
+      mapFragment.getMapAsync(onMapReadyCallback);
+        Log.d("log>>","callmap");
+
+    }
+    private void MapReady(GoogleMap googleMap){
+        Log.d("log>>","map ready");
+        mMap = googleMap;
+        MapStyleOptions styleOptions=MapStyleOptions.loadRawResourceStyle(LobbyMapActivity.this,R.raw.document);
+       mMap.setMapStyle(styleOptions);
+
+        MarkerOptions markerOptions=new MarkerOptions();
+        markerOptions.position(new LatLng(44.968046,-94.420307));
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(44.968046  ,-94.420307)));
+    }
+    private void MapInitialise(){
+        new Thread(() -> {
+            try {
+                mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+                callIT();
+
+            }
+            catch (Exception ignored){
+            }
+        }).start();
+    }
+
+    private void callIT(){
+
     }
 
     public void MapClicked(View view) {
